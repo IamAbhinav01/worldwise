c1fcfcbf760a9fd92ab6b3a7c3e5d4fa00dd058d
Abhinav <AbhinavSunil@hotmail.com>
2026-01-03 22:54:24 +0530

    initialised the project

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import AppNav from '../components/AppNav';
+
+function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>Welcome to the App Layout page.</p>
+    </div>
+  );
+}
+
+export default AppLayout;
