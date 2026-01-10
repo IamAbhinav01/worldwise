163227c3eabcb74ce591923be2d1f2e75b30c08a
Abhinav <AbhinavSunil@hotmail.com>
2026-01-10 22:20:36 +0530

    working

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,11 @@
-import AppNav from '../components/AppNav';
-
+import Sidebar from '../components/Sidebar';
+import styles from './AppLayout.module.css';
+import Map from '../components/Map';
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>Welcome to the App Layout page.</p>
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
     </div>
   );
 }
